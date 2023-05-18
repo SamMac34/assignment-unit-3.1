b51cf90d2b82b7e7782944925e53a7154bd53416
Sam McCauley <sammccauley34@gmail.com>
2023-05-18 15:59:27 -0500

    Completed question 3 stretch goal and proofread assignment

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -33,11 +33,12 @@ console.log('First animal is', animalArray[0]);
 console.log('Second animal is', animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index 
-console.log('Last food is', foodArray[3]);
+console.log('Last animal is', animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length, 
 //      instead of the exact index number of the last item
-console.log('Last animal is', animalArray.length - 1);
+const lastAnimal = animalArray[animalArray.length - 1];
+console.log('Last animal is', lastAnimal);
 
 // 4. Adding & Removing Array Items
 console.log('--- 4. Adding and removing array items ---');
